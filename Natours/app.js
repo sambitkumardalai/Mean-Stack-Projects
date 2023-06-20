d8c8f981ae1426b22eb4a32315fb4735501ba324
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2023-06-20 05:45:00 +0530

    global error handler

--- Natours/app.js
+++ Natours/app.js
@@ -3,7 +3,8 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 // 1) MIDDLEWARES
@@ -30,9 +31,8 @@ app.use('/api/v1/users', userRouter);
 
 // Handling unhandled routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on the server!`, 404));
 });
+
+app.use(globalErrorHandler);
 module.exports = app;
